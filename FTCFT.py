227819fd60505806a4b30bad2cb65a9264943543
dovintc-off <dovintc@gmail.com>
2026-06-04 03:36:36 +0300

    feat: add arrow controls (right, left)

--- FTCFT.py
+++ FTCFT.py
@@ -2,8 +2,9 @@ import msvcrt
 import os
 import sys
 import time
+from itertools import chain
 from decorators import WindowsOnly
-from tool import rprint, getByteKey
+from tool import getByteKey, stdout, savedata, cleardata
 
 d = False
 if "-d" in sys.argv: d = True
@@ -19,13 +20,13 @@ bufferl = []
 # DEBUG 
 while d:
     char = msvcrt.getch()
+    print(char)
     
     if char == b'\x08':
         if len(bufferl) > 0:
             bufferl.pop()
-            sys.stdout.write("\b \b")
-            sys.stdout.flush()
-    elif char == b'\r': break
+            stdout("\b \b")
+    elif char == b'\r' or char == b'\x03': break
     elif char == b'\xe0' or char == b'\x00':
         msvcrt.getch()
         continue
@@ -38,24 +39,33 @@ while d:
                 sys.stdout.flush()
         except: pass
 print("".join(bufferl)) 
-    
-def inputWithC2FT(start_up_text = None, key_stop_byte = b'\r', key_stop = "enter"):
-    if start_up_text != None:
-        print(start_up_text)
-    buffer = []
-    while True:
-        char = msvcrt.getch()
-        
-        if char == getByteKey(key_stop) or char == key_stop_byte:
-            return "".join(buffer)
-        
-        if char == b'\x08':
-            if len(buffer) > 0:
-                buffer.pop()
-                sys.stdout.write("\b \b")
-                sys.stdout.flush()
 
-        if char == b'\xe0' or char == b'\x00':
+@WindowsOnly
+def inputPath(start_up_text = None, key_stop_byte = b'\r', key_stop = "enter"):
+    if start_up_text != None:
+        print(start_up_text, end="", flush=True)
+    cleardata()
+    buffer = [[], []]
+    cursor = 0
+        
+    while True:
+        text = ""
+        char = msvcrt.getch()
+        STOP_SIGNALS = (getByteKey(key_stop), key_stop_byte, b'\x03')
+        if char in STOP_SIGNALS:
+            text += ", BREAK!"
+            return "".join(chain(buffer[0], "|", buffer[1]))
+        
+        elif char == b'\x08':
+            if len(buffer[0]) > 0:
+                buffer[0].pop()
+                stdout("\b \b")
+                text += ", delete!"
+                if len(buffer[1]) > 0:
+                    stdout("".join(buffer[1]) + " ")
+                    stdout("\b" * (len(buffer[1]) + 1))
+                       
+        elif char in (b'\xe0', b'\x00'):
             arrows_keys = {
                 b'H': [0, 1],
                 b'P': [0, -1],
@@ -63,18 +73,42 @@ def inputWithC2FT(start_up_text = None, key_stop_byte = b'\r', key_stop = "enter
                 b'M': [1, 0]
             }
             next_char = msvcrt.getch()
-            continue
+            text += f", Special Symbol, next char: {next_char}"
+            if next_char in arrows_keys:
+                if (arrows_keys[next_char])[0] == -1:
+                    if len(buffer[0]) > 0:
+                        buffer[1].insert(0, buffer[0][-1])
+                        buffer[0].pop()
+                        stdout("\b")
+                        text += "  <==="
+                if (arrows_keys[next_char])[0] == 1:
+                    if len(buffer[1]) > 0:
+                        buffer[0].append(buffer[1][0])
+                        del buffer[1][0]
+                        text += "  ===>"
+                        stdout(buffer[0][-1])
+            
+        else:   
+            try:
+                symbol = char.decode("cp866")
+                if ord(symbol) > 32:
+                    buffer[0].append(symbol)
+                    cursor += 1
+                    if len(buffer[1]) == 0:
+                        sys.stdout.write(symbol)
+                        text += "Append symbol"
+                    else:
+                        stdout(symbol)
+                        stdout("".join(buffer[1]))
+                        stdout("\b" * len(buffer[1]))
+                        text += f", rewrite symbols, New: {"".join(buffer[1])}"
+                    sys.stdout.flush()
+                    
+            except UnicodeDecodeError:
+                pass
         
-        try:
-            symbol = char.decode("cp866")
-            if ord(symbol) > 32:
-                buffer.append(symbol)
-                sys.stdout.write(symbol)
-                sys.stdout.flush()
-                
-        except UnicodeDecodeError:
-            pass
+        savedata(f"char: {char}, cursor: {cursor}, bufferr: {buffer[0]}, bufferl: {buffer[1]}{text}")
         time.sleep(0.001)
                     
         
-x = inputWithC2FT()
+x = inputPath("Input Your Path: ")
--- tool.py
+++ tool.py
@@ -1,3 +1,5 @@
+import sys
+
 def getByteKey(key: str):
     KeyByte = {
         "enter": b'\r',
@@ -7,3 +9,12 @@ def getByteKey(key: str):
     return KeyByte[key]
     
 def rprint(text: str): print(f"\r{text}", end="")
+def stdout(text: str): sys.stdout.write(text); sys.stdout.flush()
+
+def savedata(data: str, file="./outdata.txt", mode='a'):
+    with open(file=file, mode=mode, encoding='utf-8') as file:
+        file.write(data + "\n")
+        
+def cleardata(file="./outdata.txt"):
+    with open(file=file, mode="w", encoding='utf-8') as file:
+        file.write("")
